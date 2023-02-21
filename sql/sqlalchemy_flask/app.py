5a72b54f4c47b76d990b4239e8817513edd93ba3
terry.schoch@gmail.com <terryschoch@Terrys-Mac-mini.local>
2023-02-21 15:15:32 -0500

    restoring data + updated csvs, datacleaning, leaflet iterations

--- sql/sqlalchemy_flask/app.py
+++ sql/sqlalchemy_flask/app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Import Dependencies
 import numpy as np
 import sqlalchemy
@@ -22,157 +21,10 @@ Base.prepare(autoload_with=engine)
 # Save reference to the table
 Airlines = Base.classes.airlines
 Airports = Base.classes.airports
-Flights = Base.classes.flights
-Routes = Base.classes.routes
-All_Flights = Base.classes.all_flights
-
-
-# Create session (link) from Python to the DB 
-session = Session(engine)
-
-
-#################################################
-# Flask Setup
-#################################################
-app = Flask(__name__)
-
-# # Create Global variables defining most recent date, date 12 months prior, and most active station
-# define function to convert ORM data into dicts using column headers as keys
-def columns_to_dict(self):
-    dict_ = {}
-    for key in self.__mapper__.c.keys():
-        dict_[key] = getattr(self, key)
-    return dict_
-
-#     # Calculate the date one year from the last date in data set.
-# prev_year = dt.datetime.strptime(mosrec_date[0], "%Y-%m-%d") - dt.timedelta(days=365)
-
-#     # Query to find the most active station
-# most_active = session.query(Measurement.station, func.count(Measurement.tobs)).\
-#     group_by(Measurement.station).\
-#     order_by(func.count(Measurement.tobs).desc()).first()
-
-
-#################################################
-# Flask Routes
-#################################################
-@app.route("/")
-def welcome():
-    """List all available api routes."""
-    return (
-        f"<h1>Welcome to Fly High Memberships!</h1>"
-        f"<h2>Where we'll take you to the best airline to take you anywhere!</h2><br><br>"
-        f"<b>Available Routes:</b><br><br>"
-        f"/api/v1.0/airlines<br>"
-        f"<i>(Information about each Airline, including: Airline Name, ID, Country of Origin, Active Status)</i><br><br>"
-
-        f"/api/v1.0/airports<br>"
-        f"<i>(A list of all the airports around the globe)</i><br><br>"
-
-        f"/api/v1.0/flights<br>"
-        f"<i>(A list of domestic flights out of NYC)</i><br><br>"
-
-        f"/api/v1.0/all_flights<br>"
-        f"<i>(A list of all flight routes out of NYC)</i><br><br>"
-        
-        f"/api/v1.0/routes<br>"
-        f"<i>(A list of all air traffic routes around the globe)</i><br><br>"
-    )
-
-
-# Setup airlines Route
-@app.route("/api/v1.0/airlines")
-def airlines():  
-    """Return a JSON representation of a dictionary for airlines"""
-    airlines_dict = [columns_to_dict(row) for row in session.query(Airlines).all()]
-    return jsonify(airlines_dict)
-    
-    # test = []
-    # Query airline_id and airline_name    
-    # airlines_info = session.query(Airlines.airline_id, Airlines.airline_name, Airlines.origin_country, Airlines.active).all()
-    # airlines_info = session.query(Airlines.airline_id).all()
-    # for u in session.query(Airlines).all():
-    #     test.a = u.__dict__
-    # airlines_info = session.query(Airlines.airline_id, Airlines.airline_name).all()
-    # Convert queried list of tuples into dictionary with date as key and prcp as value
-    # airlines_dict = dict((x, y) for x, y in airlines_info)    
-    # airlines_dict = dict(("id", x) for x in airlines_info)    
-    # airlines_list = list(np.ravel(airlines_info))
-
-    # return jsonify(airlines_list)
-    # return jsonify(test)
-
-
-# Setup airports Route
-@app.route("/api/v1.0/airports")
-def airports():  
-    """Return a JSON representation of a dictionary for airports"""
-    airports_dict = [columns_to_dict(row) for row in session.query(Airports).all()]
-    return jsonify(airports_dict)
-
-
-# Setup flights Route
-@app.route("/api/v1.0/flights")
-def flights():  
-    """Return a JSON representation of a dictionary for flights"""
-    flights_dict = [columns_to_dict(row) for row in session.query(Flights).all()]
-    return jsonify(flights_dict)
-
-
-# Setup all_flights Route
-@app.route("/api/v1.0/all_flights")
-def all_flights():  
-    """Return a JSON representation of a dictionary for flights"""
-    all_flights_dict = [columns_to_dict(row) for row in session.query(All_Flights).all()]
-    return jsonify(all_flights_dict)
-
-
-# Setup routes Route
-@app.route("/api/v1.0/routes")
-def routes():  
-    """Return a JSON representation of a dictionary for routes"""
-    routes_dict = [columns_to_dict(row) for row in session.query(Routes).all()]
-    return jsonify(routes_dict)    
-
-
-session.close()
-
-
-if __name__ == '__main__':
-=======
-# Import Dependencies
-import numpy as np
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from flask import Flask, jsonify
-
-
-#################################################
-# Database Setup
-#################################################
-engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/airtravel_db")
-
-# reflect an existing database into a new model
-Base = automap_base()
-
-# reflect the tables
-Base.prepare(autoload_with=engine)
-
-# Save reference to the table
-Airlines = Base.classes.airlines
-Airports = Base.classes.airports
-<<<<<<< HEAD
 Dom_Flights = Base.classes.dom_flights
 Int_Flights = Base.classes.int_flights
 Flight_Routes = Base.classes.flight_routes
 
-=======
-Flights = Base.classes.flights
-Routes = Base.classes.routes
-All_Flights = Base.classes.all_flights
->>>>>>> 5a3f402a91cc5842270d03164089a92a5f66bd9f
 
 
 # Create session (link) from Python to the DB 
@@ -184,21 +36,12 @@ session = Session(engine)
 #################################################
 app = Flask(__name__)
 
-# # Create Global variables defining most recent date, date 12 months prior, and most active station
 # define function to convert ORM data into dicts using column headers as keys
 def columns_to_dict(self):
     dict_ = {}
     for key in self.__mapper__.c.keys():
         dict_[key] = getattr(self, key)
     return dict_
-
-#     # Calculate the date one year from the last date in data set.
-# prev_year = dt.datetime.strptime(mosrec_date[0], "%Y-%m-%d") - dt.timedelta(days=365)
-
-#     # Query to find the most active station
-# most_active = session.query(Measurement.station, func.count(Measurement.tobs)).\
-#     group_by(Measurement.station).\
-#     order_by(func.count(Measurement.tobs).desc()).first()
 
 
 #################################################
@@ -217,7 +60,6 @@ def welcome():
         f"/api/v1.0/airports<br>"
         f"<i>(A list of all the airports around the globe)</i><br><br>"
 
-<<<<<<< HEAD
         f"/api/v1.0/dom_flights<br>"
         f"<i>(A list of domestic flights out of NYC)</i><br><br>"
 
@@ -225,15 +67,6 @@ def welcome():
         f"<i>(A list of international flight routes out of NYC)</i><br><br>"
         
         f"/api/v1.0/flight_routes<br>"
-=======
-        f"/api/v1.0/flights<br>"
-        f"<i>(A list of domestic flights out of NYC)</i><br><br>"
-
-        f"/api/v1.0/all_flights<br>"
-        f"<i>(A list of all flight routes out of NYC)</i><br><br>"
-        
-        f"/api/v1.0/routes<br>"
->>>>>>> 5a3f402a91cc5842270d03164089a92a5f66bd9f
         f"<i>(A list of all air traffic routes around the globe)</i><br><br>"
     )
 
@@ -277,7 +110,6 @@ def flights():
     return jsonify(dom_flights_dict)
 
 
-<<<<<<< HEAD
 # Setup int_flights Route
 @app.route("/api/v1.0/int_flights")
 def int_flights():  
@@ -289,19 +121,6 @@ def int_flights():
 # Setup flight_routes Route
 @app.route("/api/v1.0/flight_routes")
 def flight_routes():  
-=======
-# Setup all_flights Route
-@app.route("/api/v1.0/all_flights")
-def all_flights():  
-    """Return a JSON representation of a dictionary for flights"""
-    all_flights_dict = [columns_to_dict(row) for row in session.query(All_Flights).all()]
-    return jsonify(all_flights_dict)
-
-
-# Setup routes Route
-@app.route("/api/v1.0/routes")
-def routes():  
->>>>>>> 5a3f402a91cc5842270d03164089a92a5f66bd9f
     """Return a JSON representation of a dictionary for routes"""
     flight_routes_dict = [columns_to_dict(row) for row in session.query(Flight_Routes).all()]
     return jsonify(flight_routes_dict)    
@@ -320,5 +139,4 @@ session.close()
 
 
 if __name__ == '__main__':
->>>>>>> c47801dd2537fdc071fcdf3c42af49cba01f08a7
     app.run(debug=True)
--- visualizations/flask-java-python-test/static/js/universal.js
+++ visualizations/flask-java-python-test/static/js/universal.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 var myMap = L.map('map', {// .fitWorld();
       center: [40.7128, -74.0060],
       zoom: 11
@@ -12,104 +9,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 // fetch nyc to international airports
-fetch('/api/v1.0/nyc_airports').then(function (response) {
-// console.log(response)
-return response.json();        
-}).then(function (text) {
-console.log(text); // Print the text as JSON 
-// console.log(JSON.stringify(text)) // Prints the above as plain text in console
-for (var i = 0; i < text.length; i++) {
-    var latitude = text[i].latitude;
-    var longitude = text[i].longitude;
-    var popupText = `<strong> ${text[i].airport_name}</strong><hr>
-    <strong>Location:</strong> ${text[i].city}, ${text[i].country}<br>
-    <strong>Coordinates: </strong>${latitude}, ${longitude}<br>    
-    <strong>Altitude: </strong>${text[i].altitude}<br>`;
-    // console.log(latitude);
-    // console.log(longitude);
-    if (location) {
-    L.circleMarker([latitude, longitude], {
-        stroke: true,        
-        weight: 1,
-        color: "#000000",
-        opacity: .4,        
-        fillColor: "#0000ff",
-        fillOpacity: 0.4,
-        radius: (4)
-    }).addTo(myMap).bindPopup(popupText);          
-    }
-}      
-});
-
-
-// fetch nyc to domestic airports
-fetch('/api/v1.0/nycdom_airports').then(function (response) {
-// console.log(response)
-return response.json();        
-}).then(function (text) {
-console.log(text); // Print the text as JSON
-// console.log(JSON.stringify(text)) // Prints the above as plain text in console
-for (var i = 0; i < text.length; i++) {
-    var latitude = text[i].latitude;
-    var longitude = text[i].longitude;
-    var popupText = `<strong> ${text[i].airport_name}</strong><hr>
-    <strong>Location:</strong> ${text[i].city}, ${text[i].country}<br>
-    <strong>Coordinates: </strong>${latitude}, ${longitude}<br>    
-    <strong>Altitude: </strong>${text[i].altitude}<br>`;
-    // console.log(latitude);
-    // console.log(longitude);
-    if (location) {
-    L.circleMarker([latitude, longitude], {
-        stroke: true,        
-        weight: 1,
-        color: "#00cc00",
-        opacity: .4,        
-        fillColor: "#00ff00",
-        fillOpacity: 0.4,
-        radius: (4)
-    }).addTo(myMap).bindPopup(popupText);          
-    }
-}      
-});
-
-// Heat Map test
-fetch('/api/v1.0/nycdom_airports').then(function (response) {
-    // console.log(response)
-    return response.json();        
-    }).then(function (text) {
-    // console.log(text);  
-    var heatArray = [];  
-    for (var i = 0; i < text.length; i++) {
-        var latitude = text[i].latitude;
-        var longitude = text[i].longitude;
-  
-        if (location) {
-            heatArray.push([latitude, longitude]);
-        }
-    }
-  
-    var heat = L.heatLayer(heatArray, {
-      radius: 20,
-      blur: 5
-    }).addTo(myMap);
-=======
->>>>>>> 5a3f402a91cc5842270d03164089a92a5f66bd9f
-var myMap = L.map('map', {// .fitWorld();
-      center: [40.7128, -74.0060],
-      zoom: 11
-    });
-    
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(myMap);
-
-
-// fetch nyc to international airports
-<<<<<<< HEAD
 fetch('/api/v1.0/nycint_airports').then(function (response) {
-=======
-fetch('/api/v1.0/nyc_airports').then(function (response) {
->>>>>>> 5a3f402a91cc5842270d03164089a92a5f66bd9f
 // console.log(response)
 return response.json();        
 }).then(function (text) {
@@ -169,7 +69,6 @@ for (var i = 0; i < text.length; i++) {
 }      
 });
 
-<<<<<<< HEAD
 // fetch traffic density route
 fetch('/api/v1.0/traffic_density').then(function (response) {
 // console.log(response)
@@ -227,27 +126,3 @@ console.log(text); // Print the text as JSON
 //       blur: 5
 //     }).addTo(myMap);
 // });
-=======
-// Heat Map test
-fetch('/api/v1.0/nycdom_airports').then(function (response) {
-    // console.log(response)
-    return response.json();        
-    }).then(function (text) {
-    // console.log(text);  
-    var heatArray = [];  
-    for (var i = 0; i < text.length; i++) {
-        var latitude = text[i].latitude;
-        var longitude = text[i].longitude;
-  
-        if (location) {
-            heatArray.push([latitude, longitude]);
-        }
-    }
-  
-    var heat = L.heatLayer(heatArray, {
-      radius: 20,
-      blur: 5
-    }).addTo(myMap);
->>>>>>> c47801dd2537fdc071fcdf3c42af49cba01f08a7
-});
->>>>>>> 5a3f402a91cc5842270d03164089a92a5f66bd9f
